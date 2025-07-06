9ff8ed5eaff6437f9aa6516556cdd6a668663b40
samj <papasam0310@gmail.com>
2025-07-06 16:14:55 +0800

    init

---
+++ src/index.js
@@ -0,0 +1,4 @@
+import "./style/style.css";
+
+const add = (a, b) => a + b;
+console.log(add(4, 6));
---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.js",
+
+  output: {
+    path: path.resolve(__dirname, "DIST"),
+    filename: "[contenthash].js",
+  },
+  mode: "development",
+
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: ["babel-loader"],
+      },
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "[name].[contenthash].css",
+    }),
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+    }),
+  ],
+};
